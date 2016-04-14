426adaaea875db8e37d078fc90bd9354bed29202
Jim Fleming <jimmy.fleming@gmail.com>
2016-04-13 21:50:46 -0700

    Increase model size

--- architectures.py
+++ architectures.py
@@ -4,15 +4,7 @@ from layers import Dense, Conv2D, Flatten, Conv2DBatchNorm, AvgPool, Dropout, Ac
 
 def vgg_bn():
     return [
-        Conv2D([3, 3], 32, [1, 1, 1, 1], padding='SAME'),
-        Conv2DBatchNorm(32),
-        Activation(tf.nn.relu),
-
-        Conv2D([3, 3], 32, [1, 1, 1, 1], padding='SAME'),
-        Conv2DBatchNorm(32),
-        Activation(tf.nn.relu),
-
-        Conv2D([3, 3], 64, [1, 2, 2, 1]),
+        Conv2D([3, 3], 64, [1, 1, 1, 1], padding='SAME'),
         Conv2DBatchNorm(64),
         Activation(tf.nn.relu),
 
@@ -28,9 +20,17 @@ def vgg_bn():
         Conv2DBatchNorm(128),
         Activation(tf.nn.relu),
 
+        Conv2D([3, 3], 256, [1, 2, 2, 1]),
+        Conv2DBatchNorm(256),
+        Activation(tf.nn.relu),
+
+        Conv2D([3, 3], 256, [1, 1, 1, 1], padding='SAME'),
+        Conv2DBatchNorm(256),
+        Activation(tf.nn.relu),
+
         Flatten(),
 
-        Dense(128),
+        Dense(256),
         Activation(tf.sigmoid),
 
         Dropout(0.5),
