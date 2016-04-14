e7840152dc709640696bc20607aab943f85d7418
Jim Fleming <jimmy.fleming@gmail.com>
2016-04-13 20:51:15 -0700

    Increase filters

--- architectures.py
+++ architectures.py
@@ -4,15 +4,7 @@ from layers import Dense, Conv2D, Flatten, Conv2DBatchNorm, AvgPool, Dropout, Ac
 
 def vgg_bn():
     return [
-        Conv2D([3, 3], 16, [1, 1, 1, 1], padding='SAME'),
-        Conv2DBatchNorm(16),
-        Activation(tf.nn.relu),
-
-        Conv2D([3, 3], 16, [1, 1, 1, 1], padding='SAME'),
-        Conv2DBatchNorm(16),
-        Activation(tf.nn.relu),
-
-        Conv2D([3, 3], 32, [1, 2, 2, 1]),
+        Conv2D([3, 3], 32, [1, 1, 1, 1], padding='SAME'),
         Conv2DBatchNorm(32),
         Activation(tf.nn.relu),
 
@@ -28,9 +20,17 @@ def vgg_bn():
         Conv2DBatchNorm(64),
         Activation(tf.nn.relu),
 
+        Conv2D([3, 3], 128, [1, 2, 2, 1]),
+        Conv2DBatchNorm(128),
+        Activation(tf.nn.relu),
+
+        Conv2D([3, 3], 128, [1, 1, 1, 1], padding='SAME'),
+        Conv2DBatchNorm(128),
+        Activation(tf.nn.relu),
+
         Flatten(),
 
-        Dense(64),
+        Dense(128),
         Activation(tf.sigmoid),
 
         Dropout(0.5),
